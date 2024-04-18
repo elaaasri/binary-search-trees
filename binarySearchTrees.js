15f6e2e1851e676644e89b9f1a7133f8fc25c3ca
elaaasri <anas_elasri@outlook.fr>
2024-04-18 17:28:41 +0100

    adding prettyPrint() to visualize binary search tree structure

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -8,10 +8,11 @@ class Node {
 }
 // tree class :
 class Tree {
+  constructor(root) {
+    this.root = root || null;
+  }
   buildTree(array) {
-    // const sortedArray = [...new Set(array.sort((a, b) => a - b))];
-    // console.log(sortedArray);
-
+    const sortedArray = [...new Set(array.sort((a, b) => a - b))];
     const start = 0;
     const end = array.length - 1;
     const mid = Math.floor((start, end) / 2);
@@ -19,16 +20,34 @@ class Tree {
     // base case :
     if (start > end) return null;
     // recursive case :
-    const rootNode = new Node(array[mid]);
-    const left = array.slice(start, mid);
-    const right = array.slice(mid + 1, end + 1);
+    const rootNode = new Node(sortedArray[mid]);
+    const left = sortedArray.slice(start, mid);
+    const right = sortedArray.slice(mid + 1, end + 1);
     rootNode.left = this.buildTree(left);
     rootNode.right = this.buildTree(right);
 
+    this.root = rootNode;
     return rootNode;
   }
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
 }
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree();
 console.log(tree.buildTree(arr));
+tree.prettyPrint();
